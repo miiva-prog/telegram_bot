dd8965bb05b83ce61a8c9744a7b1e8b015ce14c9
root <root@LAPTOP-6QUNCNHT>
2023-07-16 00:52:49 +0900

    upgrade main.py

--- aiogram/main.py
+++ aiogram/main.py
@@ -47,22 +47,51 @@ async def on_startup(_):
 @disp.message_handler()
 async def echo(message:types.Message):
     await message.answer(text=message.text)
-"""
-"""
+
+
 @disp.message_handler()
 async def message_capitalize(message:types.Message):
     await message.answer(text=message.text.capitalize())
-"""
-"""
+
+
 @disp.message_handler()
 async def message_upper(message:types.Message):
     await message.answer(text=message.text.upper())
-"""
-"""
+
+
 @disp.message_handler()
 async def answer_two_words(message:types.Message):
     if message.text.count(' ') == 1:
         await message.answer(text=message.text)
+
+
+@disp.message_handler()
+async def replace_heart(message:types.Message):
+    if message.text == "❤️":
+        await message.answer(text="💔")
+
+
+@disp.message_handler()
+async def message_chat(message:types.Message):
+    await bot.send_message(chat_id=message.from_user.id,text="Good!") 
+
+
+@disp.message_handler()
+async def bold_font(message:types.Message):
+    await message.answer(text="<b>Font bold</b>",parse_mode="HTML")
+
+
+@disp.message_handler()
+async def yes_or_no(message:types.Message):
+    if '0' in message.text:
+        await message.answer(text='YES')
+    else:
+        await message.answer(text='NO')
+
+     
+@disp.message_handler()
+async def italics_font(message:types.Message):
+    await message.answer(text="<em>Font italics</em>",parse_mode="HTML")
 """
 
 @disp.message_handler(commands=['start'])
@@ -90,46 +119,17 @@ async def count_bot(message:types.Message):
     await message.answer(f'count -> {count_call}')
     await message.delete()
 
-"""
-@disp.message_handler()
-async def yes_or_no(message:types.Message):
-    if '0' in message.text:
-        await message.answer(text='YES')
-    else:
-        await message.answer(text='NO')
-"""
 
 @disp.message_handler(commands=['stiker'])
 async def stiker_bot(messange:types.Message):
     await bot.send_sticker(messange.from_user.id,sticker=configuration.HASBULLA)
 
-"""
-@disp.message_handler()
-async def bold_font(message:types.Message):
-    await message.answer(text="<b>Font bold</b>",parse_mode="HTML")
-"""
-"""
-@disp.message_handler()
-async def italics_font(message:types.Message):
-    await message.answer(text="<em>Font italics</em>",parse_mode="HTML")
-"""
 
 @disp.message_handler(commands=['heart'])
 async def heart_bot(message:types.Message):
     await message.reply(text="❤️")
     await message.delete()
 
-"""
-@disp.message_handler()
-async def replace_heart(message:types.Message):
-    if message.text == "❤️":
-        await message.answer(text="💔")
-"""
-"""
-@disp.message_handler()
-async def message_chat(message:types.Message):
-    await bot.send_message(chat_id=message.from_user.id,text="Good!") 
-"""
 
 @disp.message_handler(commands=['hedgehog'])
 async def hedgehog_bot(message:types.Message):
@@ -160,9 +160,11 @@ async def hedgehog_bot(message:types.Message):
 async def map_bot(messange:types.Message):
     await bot.send_location(chat_id=messange.chat.id,latitude=randint(1,100),longitude=randint(1,100))
 
+    
 @disp.message_handler(commands=['social_network'])
 async def social_network_bot(messange:types.Message):
     await bot.send_message(chat_id=messange.chat.id,text="My social network",reply_markup=inline)
+
 
 if __name__ == "__main__":
     executor.start_polling(disp,skip_updates=True)
